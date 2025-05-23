0158bbf9f3d0d015e8b81c3601c93b67ce77f75d
Achiack <70217903+Achiack@users.noreply.github.com>
2025-05-23 18:17:32 +0800

    added icon

--- financial-dashboard-orbital/financial-dashboard/app/sideMenu.tsx
+++ financial-dashboard-orbital/financial-dashboard/app/sideMenu.tsx
@@ -1,9 +1,13 @@
 export default function SideMenu() {
     return (
-      <nav className="w-16 h-screen bg-black text-white p-6 border-r border-gray-700">
-        <h2 className="text-2xl font-bold mb-6">Menu</h2>
+      <nav className="w-16 h-screen bg-black text-white p-4 border-r border-gray-700">
+        <h2 className="text-2xl font-bold mb-6">
+            <svg height="32" width="32">
+                <image height="32" width="32" href="/icon.jpg" />
+                </svg>
+        </h2>
         <ul className="space-y-4">
-          <li><a href="/" className="hover:text-gray-400">a</a></li>
+          <li><a href="/test" className="hover:text-gray-400">a</a></li>
           <li><a href="/about" className="hover:text-gray-400">b</a></li>
           <li><a href="/contact" className="hover:text-gray-400">c</a></li>
         </ul>
