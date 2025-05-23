93340eef56fbb3748b66b0c905817707907804b9
Achiack <70217903+Achiack@users.noreply.github.com>
2025-05-23 17:47:13 +0800

    setup nexjs

---
+++ financial-dashboard-orbital/financial-dashboard/app/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+      <header className="p-32 bg-black-500 border-b border-white text-white header">
+        <h1 className="text-xl font-bold">My App</h1>
+      </header>
+    );
+  }
---
+++ financial-dashboard-orbital/financial-dashboard/app/layout.tsx
@@ -0,0 +1,17 @@
+import '../src/app/globals.css'
+import Header from './header';
+
+export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+            <body>
+                <Header />
+                {children}
+            </body>
+      </html>
+    )
+  }
