4fe275916e11f4894f0fa0b29e04d404d71a0ec0
Achiack <70217903+Achiack@users.noreply.github.com>
2025-05-26 22:40:31 +0800

    made icon clickable and lead back to main page

--- financial-dashboard-orbital/financial-dashboard/components/sideMenu.tsx
+++ financial-dashboard-orbital/financial-dashboard/components/sideMenu.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function SideMenu() {
     return (
       <nav className="w-16 h-screen bg-black text-white p-4 border-r border-gray-700">
         <h2 className="text-2xl font-bold mb-6">
+          <Link href="/">
             <svg height="32" width="32">
                 <image height="32" width="32" href="/icon.jpg" />
                 </svg>
+                </Link>
         </h2>
         <ul className="space-y-4">
           <li><a href="/test" className="hover:text-gray-400">a</a></li>
