7b1ee3f286a32ec6388a8bd1c37de11fe909e8a4
Achiack <70217903+Achiack@users.noreply.github.com>
2025-05-28 02:07:02 +0800

    pushed api keys for firebase

--- financial-dashboard-orbital/financial-dashboard/firebase/clientApp.ts
+++ financial-dashboard-orbital/financial-dashboard/firebase/clientApp.ts
@@ -7,6 +7,17 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkgCBSYip3opQLF9SiRZRftqpRNIbwz2Y",
+  authDomain: "orbital-7327.firebaseapp.com",
+  projectId: "orbital-7327",
+  storageBucket: "orbital-7327.firebasestorage.app",
+  messagingSenderId: "1029065743145",
+  appId: "1:1029065743145:web:df5c46e3302a2867bcf538",
+};
+
+/*
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -14,7 +25,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+};*/
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
